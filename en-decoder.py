47e469f7142dba1e5dc15ce4ed319880c79f1d23
Zxmon <68187602+Zxmon-code@users.noreply.github.com>
2022-01-17 12:45:55 +0100

    added decoder function

--- en-decoder.py
+++ en-decoder.py
@@ -21,22 +21,35 @@ def encoder():
     word_encoded = base_bytes.decode('ascii')
     #print the encoded words
     print(word_encoded)
+    again_encoder()
 
-    def again_encoder():
-        again = input(str(name+" do you want to encode another word? Type yes. If you want to proceed type no: "))
+def again_encoder():
+       again = input(str(name+" do you want to encode another word? Type yes. If you want to proceed type no: "))
 
-        again_l = again.lower()
+    again_l = again.lower()
 
-        if again_l == "yes":
-            encoder()
+    if again_l == "yes":
+        encoder()
 
-        elif again_l == "no":
-            decoder()
+    elif again_l == "no":
+        decoder()
 
-        else:
-            print(name+" it seem that your typing skills are an absolute abomination. maybe you should try that again but this time type yes or no.")
+    else:
+        print(name+" it seem that your typing skills are an absolute abomination. maybe you should try that again but this time type yes or no.")
+
 #call the encoder function
 encoder()
 
- def decoder():
-     
+def decoder():
+    #ask for the string, that is supposed to be decoded
+    message_encoded = input(str(name + " what word would you like to decode?: "))
+    #turn the encoded message into base64 encodable bytes
+    message_bytes = message_encoded.encode('ascii')
+    #decode the bytes
+    message_bytes_encoded = base64.b64encode(message_bytes)
+    #turn the bytes into readable text
+    message_decoded = message_bytes_encoded.decode('ascii')
+    #print the decoded word
+    print("Ok "+name+" the string "+message_encoded+" decoded is:")
+    time.sleep(3)
+    print(message_decoded)
